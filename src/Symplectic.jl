8f8d43cc2c747cd9d90778778d9648cdb19efd24
Mengzhen-Zhang <45741082+Mengzhen-Zhang@users.noreply.github.com>
2022-09-26 20:29:24 -0500

    Clean up Symplectic.jl

--- src/Symplectic.jl
+++ src/Symplectic.jl
@@ -31,48 +31,8 @@ include("helper.jl")
 include("Func.jl")
 include("SymplecticOperation.jl")
 include("SymplecticCircuit.jl")
-# include("CoupledResonators.jl")
 
 export SymplecticForm, Ω,
-
-       # # interfaces
-       # checkSymplectic, modes, getQ, getP, ⊗, dsum, ⊕,
-
-       # # constructors
-       # localSymplecticTransformation, lstf,
-       # randomPhaseShifiting,
-       # localSymplecticMatrix,
-       # randomLocalSymplecticMatrix,
-       # beamSplitter,
-       # circulator,
-       # generify,
-       # phaseShifting,
-
-       # # algorithms and formulae implementation
-       # symplecticCayleyTransform, sct,
-       # inverseSymplecticCayleyTransform, isct,
-       # interferenceBasedSequence, ibs,
-       # decoupleSequence,
-       # teleportation,
-       # preIwasawaFactorization, pif,
-       # spectrum,
-
-       # # helpers
-       # genericity,
-       # nonSymplecticity,
-       # toQPQPBasis,
-       # toQQPPBasis,
-
-       # symplectic operation
        SymplecticOperation,
        toSympType
-
-       # # generating scatteringMatrix using coupled-mode theory
-       # CoupledResonators,
-       # addActive,
-       # addPassive,
-       # addGammaEx,
-       # addGammaIn,
-       # scatteringMatrix
-
 end
